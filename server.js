89af28c4fdbfe47a594f74c6190b5599fa3743ff
Capti <captianbeau@gmail.com>
2024-01-19 16:14:45 -0700

    added View Departments functionality

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ function menu() {
             {
                 type: 'list',
                 message: 'What would you like to do?',
-                choices: ['View Roles', 'View Department', 'View Employees', 'Update Employee Role', 'Add Employee', 'Add Role', 'Add Department', 'Exit'],
+                choices: ['View Roles', 'View Departments', 'View Employees', 'Update Employee Role', 'Add Employee', 'Add Role', 'Add Department', 'Exit'],
                 name: 'answer',
             },
         ]).then(({ answer }) => {
@@ -26,8 +26,8 @@ function menu() {
                 case 'View Roles':
                     viewRoles()
                     break;
-                case 'View Department':
-                    //View department
+                case 'View Departments':
+                    //View departments
                     break;
                 case 'View Employees':
                     //View Employees
@@ -52,6 +52,14 @@ function menu() {
 function viewRoles() {
     db.promise().query('SELECT title,salary,department.names FROM role JOIN department ON role.department_id = department.id')
         .then(([data]) => {
+            console.table(data)
+            menu()
+        })
+}
+function viewDepartments() {
+    //want to add employee last_name to this table
+    db.promise().query('SELECT names,roles.titles FROM department JOIN role ON department.id = role.department_id')
+        .then(([data]) =>{
             console.table(data)
             menu()
         })
