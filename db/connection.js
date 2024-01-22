8d18ff1b3dec322d42e0224ce29d29bdca33ae52
Capti <captianbeau@gmail.com>
2024-01-21 19:43:20 -0700

    added comments to connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,18 @@
+//call for mysql2 and dotenv
 const mysql = require('mysql2');
 require('dotenv').config()
+
+//connect to mysql for username and password
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      // MySQL username,
       user: process.env.DB_USER,
-      // TODO: Add MySQL password here
       password: process.env.DB_PASSWORD,
       database: 'regularCompany_db'
     },
-    console.log(`Connected to the movies_db database.`)
   );
 db.connect((err)=>{
     if(err) throw err
 })
+//export db
 module.exports = db
